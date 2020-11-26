c68e7783c3238f659d59823046aa5edfa1ec5d06
Pham Quan <mikun1993@gmail.com>
2020-11-27 00:52:27 +0700

    add test

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+const { expect } = require('chai');
+const starwarsName = require('./index');
+
+describe('starwars-names', () => {
+  it('should work!', () => {
+    expect(true).to.be.true;
+  })
+})
